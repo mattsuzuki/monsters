53773f88b99aaa2abd019b595fdea9d3e781f2e5
mattsuzuki <matthewjamessuzuki@gmail.com>
2022-09-08 13:07:30 -0700

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,7 @@ class App extends Component {
     return (
       <div className="App">
         <SearchBox
-          className="search-box"
+          className="monsters-search-box"
           placeholder="search"
           onChangeHandler={onSearchChange}
         />
