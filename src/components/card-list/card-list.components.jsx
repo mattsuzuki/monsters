ff34402f16d994bacf458353037c277a629b3496
mattsuzuki <matthewjamessuzuki@gmail.com>
2022-09-08 15:42:42 -0700

    style

--- src/components/card-list/card-list.components.jsx
+++ src/components/card-list/card-list.components.jsx
@@ -1,18 +1,27 @@
 import { Component } from "react";
+import './card-list.styles.css'
 
 class CardList extends Component {
-    render() {
-        const {monsters} = this.props;
-        return (
-            <div>
-                {monsters.map((monster) => (
-                    <h1 key={monster.id}>{monster.name}</h1>
-                ))}
-
+  render() {
+    const { monsters } = this.props;
+    return (
+      <div className="card-list">
+        {monsters.map((monster) => {
+          const { name, email, id } = monster;
+          return (
+            <div className="card-container" key={id}>
+              <img
+                src={`https://robohash.org/${id}?set=set6&size180x180`}
+                alt={`monster${name}`}
+              />
+              <h2>{name}</h2>
+              <h2>{email}</h2>
             </div>
-
-        );
-    }
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default CardList;
